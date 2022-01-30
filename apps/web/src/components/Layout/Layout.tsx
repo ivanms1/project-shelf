04c0f0f6e34f7526dfb5c2393dd028fd05a3e57a
Ivan Saldano <ivanms1@gmail.com>
2022-01-30 12:34:21 +0900

    add plop js and layout component

---
+++ apps/web/src/components/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { StyledLayout } from "./styles";
+
+interface LayoutProps {}
+
+const Layout = ({} : LayoutProps) => {
+  return <StyledLayout>Layout</StyledLayout>;
+};
+
+export default Layout;
