53e6ae4bb64c8b62e67de9847f4551a2f11f4f88
Ivan Saldano <ivanms1@gmail.com>
2022-02-05 16:48:24 +0900

    user get server side props in home page

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from 'next';
+import { GetServerSideProps } from 'next';
 
 import { addApolloState, initializeApollo } from 'apollo';
 import { GetApprovedProjectsQuery } from 'apollo-hooks';
@@ -7,28 +7,30 @@ import Home from '@/pages/Home';
 
 import QUERY_GET_APPROVED_PROJECTS from './project/queryGetAllApprovedProjects.graphql';
 import QUERY_GET_CURRENT_USER from '@/hooks/queryGetCurrentUser.graphql';
+import { getSession } from 'next-auth/react';
 
 export default Home;
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const client = initializeApollo();
+
   try {
-    const client = initializeApollo();
-
-    await client.query<GetApprovedProjectsQuery>({
-      query: QUERY_GET_APPROVED_PROJECTS,
-    });
-
+    const session = await getSession(context);
     await client.query({
       query: QUERY_GET_CURRENT_USER,
+      context: {
+        headers: {
+          Authorization: session.token,
+        },
+      },
     });
+  } catch (error) {}
 
-    return addApolloState(client, {
-      props: {},
-      revalidate: 60,
-    });
-  } catch (error) {
-    return {
-      props: {},
-    };
-  }
+  await client.query<GetApprovedProjectsQuery>({
+    query: QUERY_GET_APPROVED_PROJECTS,
+  });
+
+  return addApolloState(client, {
+    props: {},
+  });
 };
