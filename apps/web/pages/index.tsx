0fe081c865362dd348cfe282472b9bd4d3d6f7d7
Ivan Saldano <ivanms1@gmail.com>
2022-02-05 16:18:06 +0900

    query current user in server

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -6,6 +6,7 @@ import { GetApprovedProjectsQuery } from 'apollo-hooks';
 import Home from '@/pages/Home';
 
 import QUERY_GET_APPROVED_PROJECTS from './project/queryGetAllApprovedProjects.graphql';
+import QUERY_GET_CURRENT_USER from '@/hooks/queryGetCurrentUser.graphql';
 
 export default Home;
 
@@ -15,6 +16,10 @@ export const getStaticProps: GetStaticProps = async () => {
 
     await client.query<GetApprovedProjectsQuery>({
       query: QUERY_GET_APPROVED_PROJECTS,
+    });
+
+    await client.query({
+      query: QUERY_GET_CURRENT_USER,
     });
 
     return addApolloState(client, {
