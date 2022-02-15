d87e0bc1d0a1a82b68e99ca484d63a8405da0969
Ivan Saldano <ivanms1@gmail.com>
2022-02-15 19:17:28 +0900

    remove unused import

--- packages/ui/Button/index.tsx
+++ packages/ui/Button/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import { Loader } from '../Loader';
 
-import { StyledButton, StyledLinkButton } from './styles';
+import { StyledButton } from './styles';
 
 interface Button extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'primary' | 'secondary' | 'ghost';
