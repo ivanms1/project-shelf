46362a6bd305e1f2e90d0d4d72aa465f7e56f588
mina <mina@dfrc.ch>
2022-02-17 00:44:22 +0900

    User page styling in progress

--- apps/web/src/pages/User/index.tsx
+++ apps/web/src/pages/User/index.tsx
@@ -1,19 +1,51 @@
 import React from 'react';
 import { useGetUserForPageQuery } from 'apollo-hooks';
-import { StyledUser } from './styles';
+import {
+  StyledUser,
+  StyledAvatar,
+  StyledUserContainer,
+  StyledTitle,
+  StyledProject,
+  StyledProjectContainer,
+} from './styles';
 
-function User({ userId }) {
+function User(props) {
+  console.log('useId', props?.userId);
   const { data = {} } = useGetUserForPageQuery({
     variables: {
-      id: String(userId),
+      id: String(props?.userId),
     },
-    skip: !userId,
+    skip: !props?.userId,
   });
 
-  const [projects, user] = [data?.user.projects, data?.user];
+  const { user } = data;
+  console.log('user', user);
 
-  console.log('user', user.avatar, user.name, projects);
-  return <StyledUser>User</StyledUser>;
+  return (
+    <StyledUser>
+      <StyledUserContainer>
+        <StyledAvatar
+          src={user?.avatar}
+          alt={user?.name}
+          height={200}
+          width={200}
+        />
+        <StyledTitle>{user.name}</StyledTitle>
+      </StyledUserContainer>
+      <StyledProjectContainer>
+        {user.projects.map((project) => (
+          <StyledProject
+            key={project.id}
+            src={project.preview}
+            alt={project.title}
+            height={300}
+            width={400}
+          />
+        ))}
+      </StyledProjectContainer>
+    </StyledUser>
+  );
 }
 
 export default User;
+//ckzkzm2zd00021etjgdfh0z58
--- apps/web/src/pages/User/styles.ts
+++ apps/web/src/pages/User/styles.ts
@@ -1,3 +1,35 @@
+import Image from 'next/image';
 import { styled } from 'stitches/stitches.config';
 
-export const StyledUser = styled('div', {});
+export const StyledUser = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  padding: 26,
+});
+
+export const StyledAvatar = styled(Image, {
+  borderRadius: '50%',
+});
+export const StyledTitle = styled('p', {
+  fontWeight: 'bold',
+});
+export const StyledUserContainer = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+  padding: 26,
+});
+export const StyledProject = styled(Image, {
+  borderRadius: 10,
+});
+export const StyledProjectContainer = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'row',
+  gap: 20,
+
+  justifyContent: 'space-between',
+  padding: 26,
+});
