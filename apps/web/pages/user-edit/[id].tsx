1f3cea82de78ca430e3bcb7dd86a52e24a8f2a08
miroz devkota <mirozxy@gmail.com>
2022-11-30 15:10:33 +0545

    Fix: Remove SSR functionality from the page

--- apps/web/pages/user-edit/[id].tsx
+++ apps/web/pages/user-edit/[id].tsx
@@ -1,35 +1,3 @@
 import UserEdit from '@/pages/UserEdit';
-import { addApolloState, initializeApollo } from 'apollo';
-import { getSession } from 'next-auth/react';
-import { GetServerSideProps } from 'next';
-
-import QUERY_GET_USER_FOR_PAGE from '../user/queryGetUserForPage.graphql';
 
 export default UserEdit;
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  try {
-    const client = initializeApollo();
-    const session = await getSession(ctx);
-
-    await client.query({
-      query: QUERY_GET_USER_FOR_PAGE,
-      variables: {
-        id: ctx.params?.id,
-      },
-      context: {
-        headers: {
-          Authorization: session?.token,
-        },
-      },
-    });
-
-    return addApolloState(client, {
-      props: {},
-    });
-  } catch (error) {
-    return {
-      props: {},
-    };
-  }
-};
