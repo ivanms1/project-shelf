10808de1c7ee78960dd418c5bd4244719be2e05d
Ivan Saldano <ivanms1@gmail.com>
2022-12-25 21:37:45 +0700

    fix: remove unnecessary changes

--- apps/web/src/pages/Project/Project.css.ts
+++ apps/web/src/pages/Project/Project.css.ts
@@ -39,9 +39,6 @@ export const modalStyle = style({
   marginTop: 30,
   paddingBottom: '2rem',
 
-  height: 'calc(100vh - 30px)',
-  padding: '40px 360px',
-
   '@media': {
     [media.desktop]: {
       height: 'calc(100vh - 30px)',
