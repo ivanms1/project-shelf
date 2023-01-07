a4146a09fa9d9b2e813955df3516914cb6dce8de
miroz devkota <mirozxy@gmail.com>
2023-01-07 17:23:34 +0545

    Add : i18n configuration

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,32 +1,34 @@
 const withTM = require('next-transpile-modules')(['ui']);
-const {
-  createVanillaExtractPlugin
-} = require('@vanilla-extract/next-plugin');
+const { i18n } = require('./next-i18next.config.js');
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
-module.exports = withVanillaExtract(withTM({
-  reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    config.module.rules.push({
-      test: /\.(graphql|gql)$/,
-      exclude: /node_modules/,
-      loader: 'graphql-tag/loader',
-    });
-    return config;
-  },
-  images: {
-    domains: [
-      'res.cloudinary.com',
-      'avatars.githubusercontent.com',
-      'cloudflare-ipfs.com',
-      'loremflickr.com',
-      'github.com',
-      'github.githubassets.com'
-    ],
-  },
-}));
+module.exports = withVanillaExtract(
+  withTM({
+    reactStrictMode: true,
+    i18n,
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+      config.module.rules.push({
+        test: /\.(graphql|gql)$/,
+        exclude: /node_modules/,
+        loader: 'graphql-tag/loader',
+      });
+      return config;
+    },
+    images: {
+      domains: [
+        'res.cloudinary.com',
+        'avatars.githubusercontent.com',
+        'cloudflare-ipfs.com',
+        'loremflickr.com',
+        'github.com',
+        'github.githubassets.com',
+      ],
+    },
+  })
+);
