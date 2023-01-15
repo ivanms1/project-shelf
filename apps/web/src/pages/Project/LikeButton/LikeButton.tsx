1c4c2e0e466511e76a727313418641a4c81dc9fc
Rohil pinto <rohilcrispinto@gmail.com>
2023-01-15 16:32:56 +0530

    feat: add login modal in like button

--- apps/web/src/pages/Project/LikeButton/LikeButton.tsx
+++ apps/web/src/pages/Project/LikeButton/LikeButton.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   ProjectActions,
   useGetProjectLikedStatusQuery,
@@ -6,10 +7,15 @@ import {
 import classNames from 'classnames';
 import { Button } from 'ui';
 
+import useIsLoggedIn from './../../../hooks/useIsLoggedIn';
+
 import { baseLikeButtonStyle, notLikedButtonStyle } from './LikeButton.css';
+import LoginModal from '@/components/Modals/LoginModal';
 
 function LikeButton({ projectId }: { projectId: string }) {
   const [reactToProject] = useReactToProjectMutation();
+  const { isLoggedIn } = useIsLoggedIn();
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
 
   const { data } = useGetProjectLikedStatusQuery({
     variables: {
@@ -22,38 +28,48 @@ function LikeButton({ projectId }: { projectId: string }) {
   const isLiked = data?.project?.isLiked;
 
   const handleLike = async () => {
-    try {
-      await reactToProject({
-        variables: {
-          input: {
-            projectId: projectId,
-            action: isLiked ? ProjectActions.Dislike : ProjectActions.Like,
+    if (isLoggedIn) {
+      try {
+        await reactToProject({
+          variables: {
+            input: {
+              projectId: projectId,
+              action: isLiked ? ProjectActions.Dislike : ProjectActions.Like,
+            },
           },
-        },
-        optimisticResponse: {
-          reactToProject: {
-            ...data?.project,
-            id: data?.project?.id,
-            likesCount: isLiked
-              ? data?.project.likesCount - 1
-              : data?.project.likesCount + 1,
-            isLiked: !isLiked,
+          optimisticResponse: {
+            reactToProject: {
+              ...data?.project,
+              id: data?.project?.id,
+              likesCount: isLiked
+                ? data?.project.likesCount - 1
+                : data?.project.likesCount + 1,
+              isLiked: !isLiked,
+            },
           },
-        },
-      });
-    } catch (error) {
-      // TODO: Handle error
+        });
+      } catch (error) {
+        // TODO: Handle error
+      }
+    } else {
+      setIsLoginModalOpen(true);
     }
   };
   return (
-    <Button
-      className={classNames(baseLikeButtonStyle, {
-        [notLikedButtonStyle]: !isLiked,
-      })}
-      onClick={handleLike}
-    >
-      {isLiked ? 'Liked' : 'Like'}
-    </Button>
+    <>
+      <LoginModal
+        isOpen={isLoginModalOpen}
+        onClose={() => setIsLoginModalOpen(false)}
+      />
+      <Button
+        className={classNames(baseLikeButtonStyle, {
+          [notLikedButtonStyle]: !isLiked,
+        })}
+        onClick={handleLike}
+      >
+        {isLiked ? 'Liked' : 'Like'}
+      </Button>
+    </>
   );
 }
 
