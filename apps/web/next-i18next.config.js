001fc1c8a4e6404572a9a1e8a21186a3c64de0ae
Ivan Saldano <ivanms1@gmail.com>
2023-01-16 01:39:10 +0900

    fix: add locale path

--- apps/web/next-i18next.config.js
+++ apps/web/next-i18next.config.js
@@ -1,7 +1,12 @@
+const path = require("path");
 
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'hi', 'ne', 'ko', 'es'],
   },
+  localePath:
+    typeof window === "undefined"
+      ? path.resolve("./public/locales")
+      : "/public/locales",
 };
