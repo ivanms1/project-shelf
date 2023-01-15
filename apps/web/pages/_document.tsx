9e5cd3a0f2d112e537105526d1e3e4eeba672178
Ivan Saldano <ivanms1@gmail.com>
2023-01-16 00:58:09 +0900

    fix: remove unnecessary changes in document.tsx

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 
-import i18nextConfig from '../next-i18next.config';
-
 export default class Document extends NextDocument {
   render() {
-    const currentLocale =
-      this.props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale;
-
     return (
-      <Html lang={currentLocale}>
+      <Html>
         <Head />
         <body>
           <Main />
