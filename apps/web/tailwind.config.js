6afb71a69e5c396e8a1de93ebb3793ba5fc04089
rohilpinto <rohilcrispinto@gmail.com>
2023-01-24 19:34:40 +0530

    feat: initial setup (#123)

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,6 @@
+const config = require('config/tailwind.config');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  ...config,
+};
---
+++ packages/config/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#A259FF',
+        'blue-light': '#377DF7',
+        black: '#2B2B2B',
+        'grey-dark': '#3B3B3B',
+        'grey-light': '#858584',
+        white: '#FFFFFF',
+        'red-light': '#FF6250',
+      },
+      backgroundImage: {
+        'gradient-primary': 'linear-gradient(128.15deg, #A259FF 49.75%, #377DF7 136.56%);',
+        'gradient-secondary': 'linear-gradient(100.92deg, #A259FF 13.57%, #FF6250 97.65%);',
+      },
+    },
+  },
+  content: ['../../packages/ui/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  plugins: [],
+};
