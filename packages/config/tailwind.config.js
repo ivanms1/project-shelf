24432fe059c6bf82ca6f88125b7faf5bbcba9408
Rohil pinto <rohilcrispinto@gmail.com>
2023-01-28 14:38:31 +0530

    chore: update tailwind config

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -8,9 +8,11 @@ module.exports = {
         'blue-light': '#377DF7',
         black: '#2B2B2B',
         'grey-dark': '#3B3B3B',
+        'grey-lighter': '#9e9ea7',
         'grey-light': '#858584',
         white: '#FFFFFF',
         'red-light': '#FF6250',
+        'pink-light': '#ea4c89',
       },
       backgroundImage: {
         'gradient-primary': 'linear-gradient(128.15deg, #A259FF 49.75%, #377DF7 136.56%);',
@@ -19,7 +21,14 @@ module.exports = {
       zIndex: {
         'loading-overlay': '999',
         'modal-overlay': '99999',
-      }
+      },
+      borderRadius: {
+        sm: '8px',
+        md: '12px',
+        lg: '18px',
+        xl: '30px',
+        circle: '50%',
+      },
     },
   },
   content: ['../../packages/ui/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
