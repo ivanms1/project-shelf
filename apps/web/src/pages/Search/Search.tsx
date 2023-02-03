34ea15d424b036aca716e677ba8df5cb535fa753
Iván <ivanms1@gmail.com>
2023-02-03 23:06:41 +0900

    Feat: search page revamp (#133)

--- apps/web/src/pages/Search/Search.tsx
+++ apps/web/src/pages/Search/Search.tsx
@@ -7,17 +7,6 @@ import { useTranslation } from 'next-i18next';
 
 import ProjectsGrid from '@/components/ProjectsGrid';
 
-import {
-  headerStyle,
-  projectsGridStyle,
-  resultsTitleStyle,
-  searchFormStyle,
-  searchInputStyle,
-  subtitleStyle,
-  titleStyle,
-  totalCountStyle,
-} from './Search.css';
-
 function Search() {
   const { query, push } = useRouter();
 
@@ -27,7 +16,7 @@ function Search() {
 
   const search = query?.search?.[0];
 
-  const { loading, data, fetchMore } = useSearchProjectsQuery({
+  const { loading, data, fetchMore, called } = useSearchProjectsQuery({
     variables: {
       input: {
         search,
@@ -61,38 +50,50 @@ function Search() {
   };
 
   return (
-    <div>
-      <div className={headerStyle}>
-        <h1 className={titleStyle}>{t('search')}</h1>
-        <p className={subtitleStyle}>{t('subtitle')}</p>
+    <div className='bg-black flex flex-col px-28 pt-20 pb-[3px] max-lg:px-[30px]'>
+      <div className='flex flex-col gap-3 mb-8'>
+        <h1 className='font-semibold text-5xl'>{t('title')}</h1>
+        <p className='text-[22px]'>{t('subtitle')}</p>
       </div>
-      <form className={searchFormStyle} onSubmit={handleSubmit(handleSearch)}>
+      <form
+        className='mb-20 max-lg:mb-10'
+        onSubmit={handleSubmit(handleSearch)}
+      >
         <input
-          className={searchInputStyle}
+          className='p-5 bg-black text-white border border-grey-dark rounded-lg w-full'
           name='project-search'
-          placeholder={t('Search')}
+          placeholder={t('search-placeholder')}
           defaultValue={search}
           {...register('search')}
         />
       </form>
-      {!!search && (
+      {!!search && !!data?.searchProjects?.totalCount && (
         <>
-          <h1 className={resultsTitleStyle}>{search}</h1>
-          <p className={totalCountStyle}>
-            {t('search-total', {
-              count: data?.searchProjects?.totalCount ?? 0,
-            })}
-          </p>
+          <div className='flex gap-4 px-48 w-fit py-4 border-b-[2px] border-b-grey-light justify-center items-center'>
+            <p className='capitalize text-[22px]'>{t('projects')}</p>
+            <p className='bg-grey-light rounded-lg px-4 py-[5px] font-mono'>
+              {data?.searchProjects?.totalCount}
+            </p>
+          </div>
+          <div className='bg-grey-dark py-14 px-28 max-lg:px-[30px] -mx-28 max-lg:mx-[-30px]'>
+            <ProjectsGrid
+              projects={data?.searchProjects?.results ?? []}
+              nextCursor={data?.searchProjects?.nextCursor}
+              onRefetch={onRefetch}
+              loading={loading}
+            />
+          </div>
         </>
       )}
-      <div className={projectsGridStyle}>
-        <ProjectsGrid
-          projects={data?.searchProjects?.results ?? []}
-          nextCursor={data?.searchProjects?.nextCursor}
-          onRefetch={onRefetch}
-          loading={loading}
-        />
-      </div>
+      {data?.searchProjects?.totalCount === 0 && called && (
+        <div className='flex flex-col gap-4 items-center'>
+          <p className='text-[22px]'>{t('no-results')}</p>
+          <p className='text-lg text-grey-light'>
+            {t('no-results-description')}
+          </p>
+        </div>
+      )}
+
       <NextSeo title={t('seo-title')} />
     </div>
   );
