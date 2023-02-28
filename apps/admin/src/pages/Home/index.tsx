92daaa0887e3565222a65fba9a88cb68933f438d
miroz devkota <mirozxy@gmail.com>
2023-02-28 12:12:40 +0545

    removed the comments

--- apps/admin/src/pages/Home/index.tsx
+++ apps/admin/src/pages/Home/index.tsx
@@ -2,22 +2,9 @@ import React from 'react';
 
 import { NextSeo } from 'next-seo';
 
-// import { useGetAllProjectsQuery } from 'apollo-hooks';
-
 function Home() {
-  // const { data } = useGetAllProjectsQuery();
-
   return (
     <div className='bg-gray-200'>
-      {/* 
-      <div>
-      {data?.projects.results?.map((project) => (
-        <div key={project.id}>
-        <p>{project.title}</p>
-        </div>
-        ))}
-      </div> */}
-
       <NextSeo title='Admin | Home' />
     </div>
   );
