bff55932d661c2ba68552be21bee794c592d4322
miroz devkota <mirozxy@gmail.com>
2023-03-15 14:47:59 +0545

    add: add image url support

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,36 +5,35 @@
 const withTM = require('next-transpile-modules')(['ui']);
 const { i18n } = require('./next-i18next.config.js');
 
-
-
-module.exports = withTM(
-  {
-    reactStrictMode: true,
-    experimental: {
-      newNextLinkBehavior: true,
-      scrollRestoration: true,
-    },
-    i18n,
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-      config.module.rules.push({
-        test: /\.(graphql|gql)$/,
-        exclude: /node_modules/,
-        loader: 'graphql-tag/loader',
-      });
-      return config;
-    },
-    images: {
-      domains: [
-        'res.cloudinary.com',
-        'cloudflare-ipfs.com',
-        'avatars.githubusercontent.com',
-        'loremflickr.com',
-        'via.placeholder.com'
-      ],
-    },
-  }
-);
+module.exports = withTM({
+  reactStrictMode: true,
+  experimental: {
+    newNextLinkBehavior: true,
+    scrollRestoration: true,
+  },
+  i18n,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader',
+    });
+    return config;
+  },
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'cloudflare-ipfs.com',
+      'avatars.githubusercontent.com',
+      'loremflickr.com',
+      'via.placeholder.com',
+      'github.com',
+      'github.githubassets.com',
+      'loremflickr.com',
+    ],
+  },
+});
