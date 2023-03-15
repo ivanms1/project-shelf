17a398a2137ec389fde9efb7611cb1d74d237fc1
miroz devkota <mirozxy@gmail.com>
2023-03-15 14:48:39 +0545

    add: make image url support

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -18,8 +18,11 @@ module.exports = withTM({
     domains: [
       'res.cloudinary.com',
       'avatars.githubusercontent.com',
+      'cloudflare-ipfs.com',
+      'loremflickr.com',
       'github.com',
       'github.githubassets.com',
+      'loremflickr.com',
     ],
   },
 });
