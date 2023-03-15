0c32ffbdb69885a4f17838a07f31435a3ae62730
miroz devkota <mirozxy@gmail.com>
2023-03-15 14:54:07 +0545

    minor ui designs to login page

--- apps/admin/src/pages/Login/index.tsx
+++ apps/admin/src/pages/Login/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { signIn } from 'next-auth/react';
+import { NextSeo } from 'next-seo';
 
 import { Button } from 'ui';
 
 function Login() {
   return (
-    <div>
-      <Button onClick={() => signIn()}>Login</Button>
+    <div className='h-screen flex justify-center items-center'>
+      <div className='flex flex-col'>
+        <span className='text-gray-900 text-[30px] font-bold'>Get Started</span>
+        <Button onClick={() => signIn()}>Login</Button>
+      </div>
+
+      <NextSeo title='Login | Admin' />
     </div>
   );
 }
