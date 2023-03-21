f496614d6de2e54b5af0b2f47d347aec361e017c
miroz devkota <mirozxy@gmail.com>
2023-03-21 15:03:46 +0545

    remove SSR for users page

--- apps/admin/pages/users/index.tsx
+++ apps/admin/pages/users/index.tsx
@@ -1,31 +1,3 @@
-import { addApolloState, initializeApollo } from 'apollo';
 import Users from '@/pages/Users';
 
 export default Users;
-
-import QUERY_GET_USERS_ADMIN from './queryGetUsers.graphql';
-
-export async function getStaticProps() {
-  try {
-    const client = initializeApollo();
-
-    const data = await client.query({
-      query: QUERY_GET_USERS_ADMIN,
-    });
-
-    return addApolloState(client, {
-      props: {
-        users: data?.data?.getUsers,
-      },
-      revalidate: 60,
-    });
-  } catch (error) {
-    console.log('error', error);
-
-    return {
-      props: {
-        data: error.message,
-      },
-    };
-  }
-}
