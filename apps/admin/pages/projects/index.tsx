d0d1f62bc924f158841a4b6e03a352b049433475
miroz devkota <mirozxy@gmail.com>
2023-03-21 15:03:31 +0545

    removed SSR for projects page

--- apps/admin/pages/projects/index.tsx
+++ apps/admin/pages/projects/index.tsx
@@ -1,29 +1,3 @@
-import { addApolloState, initializeApollo } from 'apollo';
 import Projects from '@/pages/Projects';
 
-import QUERY_GET_PROJECTS_ADMIN from './queryGetProjectsAdmin.graphql';
-
 export default Projects;
-
-export async function getStaticProps() {
-  try {
-    const client = initializeApollo();
-
-    const data = await client.query({
-      query: QUERY_GET_PROJECTS_ADMIN,
-    });
-
-    return addApolloState(client, {
-      props: {
-        projects: data?.data?.getProjectsAdmin?.results,
-      },
-      revalidate: 60,
-    });
-  } catch (error) {
-    return {
-      props: {
-        data: error.message,
-      },
-    };
-  }
-}
