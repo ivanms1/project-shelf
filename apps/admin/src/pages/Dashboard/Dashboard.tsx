822a26ebd5aaf2cd91b6f230bfcf1d422265b3ce
miroz devkota <mirozxy@gmail.com>
2023-03-28 10:01:15 +0545

    dashboard page changed

--- apps/admin/src/pages/Dashboard/Dashboard.tsx
+++ apps/admin/src/pages/Dashboard/Dashboard.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
 
-function Index() {
-  return (
-    <div className='w-full h-full bg-white p-[30px]'>
-      <p className='text-gray-900 text-3xl font-bold'>Dashboard</p>
+import BarGraph from '../../components/BarGraph';
+import { useGetAllUsersQuery, useGetProjectsAdminQuery } from 'apollo-hooks';
 
-      <div className='py-[60px] flex flex-row gap-[100px]'>
+function Index() {
+  const { data } = useGetAllUsersQuery({
+    fetchPolicy: 'network-only',
+  });
+  const { data: projectsAdminData } = useGetProjectsAdminQuery();
+
+  console.log({ data });
+
+  return (
+    <div className='w-full h-full  flex flex-col border-2'>
+      {/* <p className='text-gray-900 text-3xl font-bold'>Dashboard</p> */}
+
+      <div className=' flex flex-row gap-[100px] pb-[40px]'>
         <div className=' h-[266px] w-[232px] py-[20px] px-[60px] bg-white flex flex-col items-center justify-center gap-[20px] rounded-[14px] border-2 border-[#72767C]-200'>
           <span className='w-[40px] h-[40px] rounded-circle flex items-center justify-center bg-red-200 '></span>
 
-          <div className='flex flex-col gap-[10px] items-center'>
-            <span className='text-[24px] text-[#242731] font-bold'>Users</span>
-            <span className='text-[24px] text-[#242731] font-bold'>120</span>
+          <div className='flex flex-col items-center gap-[10px] '>
+            <span className='text-[24px] text-[#242731] text-center'>
+              Total Users
+            </span>
+            <span className='text-[24px] text-[#242731] '>
+              {data?.getAllUsers?.totalCount}
+            </span>
           </div>
         </div>
 
@@ -20,13 +34,50 @@ function Index() {
           <span className='w-[40px] h-[40px] rounded-circle flex items-center justify-center bg-red-200 '></span>
 
           <div className='flex flex-col gap-[10px] items-center'>
-            <span className='text-[24px] text-[#242731] font-bold'>
-              Projects
+            <span className='text-[20px] text-[#242731] text-center'>
+              Banned Users
             </span>
-            <span className='text-[24px] text-[#242731] font-bold'>1000</span>
+            <span className='text-[24px] text-[#242731] '>
+              {data?.getAllUsers?.bannedUsers}
+            </span>
+          </div>
+        </div>
+
+        <div className=' h-[266px] w-[232px] py-[20px] px-[60px] bg-white flex flex-col items-center justify-center gap-[20px] rounded-[14px] border-2 border-[#72767C]-200'>
+          <span className='w-[40px] h-[40px] rounded-circle flex items-center justify-center bg-red-200 '></span>
+
+          <div className='flex flex-col gap-[10px] items-center'>
+            <span className='text-[20px] text-[#242731] text-center'>
+              Total Projects
+            </span>
+            <span className='text-[24px] text-[#242731]'>
+              {projectsAdminData?.getProjectsAdmin?.totalCount}
+            </span>
           </div>
         </div>
       </div>
+
+      <div className='flex flex-row gap-[50px] h-[100%] w-[100%] '>
+        <div className='flex flex-col h-[100%] w-[100%] gap-[20px] bg-white rounded-[14px] py-[22px] px-[24px]'>
+          <div className='flex flex-row text-[#242731] text-[20px] '>
+            <span className='font-bold'>
+              User <span className='font-normal'>Statistics</span>
+            </span>
+          </div>
+          <BarGraph fillColor='#FF764C' />
+        </div>
+
+        <div className='flex flex-col h-[100%] w-[100%] gap-[20px] bg-white rounded-[14px] py-[22px] px-[24px]'>
+          <div className='flex flex-row text-[#242731] text-[20px] '>
+            <span className='font-bold'>
+              Project <span className='font-normal'>Statistics</span>
+            </span>
+          </div>
+          <BarGraph fillColor='#FF764C' />
+        </div>
+      </div>
+
+      {/* </div> */}
       <NextSeo title='Dashboard' />
     </div>
   );
