2f30b8f8068acc9cd6901d327e4cfab5b68ce2fe
miroz devkota <mirozxy@gmail.com>
2023-04-08 10:21:51 +0545

    debounce function

---
+++ apps/admin/src/components/Table/DebouncedInput.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+export default useDebounce;
