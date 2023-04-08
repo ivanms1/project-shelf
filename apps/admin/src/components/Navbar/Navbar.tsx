5cdc4cb15bd760704fb18a2ce4c9a4c2bba529d6
miroz devkota <mirozxy@gmail.com>
2023-04-08 10:16:12 +0545

    removed unused text

--- apps/admin/src/components/Navbar/Navbar.tsx
+++ apps/admin/src/components/Navbar/Navbar.tsx
@@ -7,8 +7,7 @@ const Navbar = () => {
   const { currentUser } = useIsLoggedIn();
 
   return (
-    <div className='bg-white w-full h-20 flex flex-row justify-between items-center px-20'>
-      <div>search</div>
+    <div className='bg-white w-full h-20 flex flex-row justify-end items-center px-20'>
       <div className='rounded-full overflow-hidden w-[50px] h-[50px]'>
         <Image
           className='w-full h-full cursor-pointer rounded-full object-cover'
