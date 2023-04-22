4c01104f46bed3b34bc100e646305cd4c530fdac
Iván <ivanms1@gmail.com>
2023-04-22 10:36:31 +0900

    feat: bring back ssr to top pages (#164)

--- apps/web/pages/top-projects/index.tsx
+++ apps/web/pages/top-projects/index.tsx
@@ -1,14 +1,40 @@
 import TopProjects from '@/pages/TopProjects';
+import { addApolloState, initializeApollo } from 'apollo';
+import {
+  GetTopProjectsDocument,
+  GetTopProjectsQuery,
+  GetTopProjectsQueryVariables,
+} from 'apollo-hooks';
 
-import type { GetStaticProps } from 'next';
+import type { GetServerSideProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import { IntervalLabels, INTERVALS } from 'const';
 
 export default TopProjects;
 
-export const getStaticProps: GetStaticProps = async ({ locale }) => {
-  return {
+export const getServerSideProps: GetServerSideProps = async ({
+  locale,
+  query,
+  res,
+}) => {
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=10, stale-while-revalidate=59'
+  );
+  const client = initializeApollo();
+
+  await client.query<GetTopProjectsQuery, GetTopProjectsQueryVariables>({
+    query: GetTopProjectsDocument,
+
+    variables: {
+      interval: INTERVALS[query.interval as IntervalLabels] ?? '',
+    },
+  });
+
+  return addApolloState(client, {
     props: {
       ...(await serverSideTranslations(locale, ['top', 'common'])),
     },
-  };
+  });
 };
