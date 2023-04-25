2415fee07f405f3660df214b349d9185206d27a3
miroz devkota <mirozxy@gmail.com>
2023-04-25 09:41:15 +0545

    fix css

--- apps/web/src/pages/Search/Search.tsx
+++ apps/web/src/pages/Search/Search.tsx
@@ -50,52 +50,54 @@ function Search() {
   };
 
   return (
-    <div className='bg-black flex flex-col px-28 pt-20 pb-[3px] max-lg:px-[30px] min-h-[52vh] max-lg:min-h-[70vh]'>
-      <div className='flex flex-col gap-3 mb-8'>
-        <h1 className='font-semibold text-5xl'>{t('title')}</h1>
-        <p className='text-[22px]'>{t('subtitle')}</p>
-      </div>
-      <form
-        className='mb-20 max-lg:mb-10'
-        onSubmit={handleSubmit(handleSearch)}
-      >
-        <input
-          className='p-5 bg-black text-white border border-grey-dark rounded-lg w-full focus:outline-none'
-          name='project-search'
-          placeholder={t('search-placeholder')}
-          autoFocus
-          defaultValue={search}
-          {...register('search')}
-        />
-      </form>
-      {!!search && !!data?.searchProjects?.totalCount && (
-        <>
-          <div className='flex gap-4 px-48 w-fit py-4 border-b-[2px] border-b-grey-light justify-center items-center max-lg:px-7'>
-            <p className='capitalize text-[22px]'>{t('projects')}</p>
-            <p className='bg-grey-light rounded-lg px-4 py-[5px] font-mono'>
-              {data?.searchProjects?.totalCount}
+    <div className='bg-black  flex justify-center'>
+      <div className=' w-full max-w-[70vw] flex flex-col px-28 pt-20 pb-[3px] max-lg:px-[30px] max-lg:min-h-[70vh]'>
+        <div className='flex flex-col gap-3 mb-8'>
+          <h1 className='font-semibold text-5xl'>{t('title')}</h1>
+          <p className='text-[22px]'>{t('subtitle')}</p>
+        </div>
+        <form
+          className='mb-20 max-lg:mb-10'
+          onSubmit={handleSubmit(handleSearch)}
+        >
+          <input
+            className='p-5 bg-black text-white border border-grey-dark rounded-lg w-full focus:outline-none'
+            name='project-search'
+            placeholder={t('search-placeholder')}
+            autoFocus
+            defaultValue={search}
+            {...register('search')}
+          />
+        </form>
+        {!!search && !!data?.searchProjects?.totalCount && (
+          <>
+            <div className='flex gap-4 px-48 w-fit py-4 border-b-[2px] border-b-grey-light justify-center items-center max-lg:px-7'>
+              <p className='capitalize text-[22px]'>{t('projects')}</p>
+              <p className='bg-grey-light rounded-lg px-4 py-[5px] font-mono'>
+                {data?.searchProjects?.totalCount}
+              </p>
+            </div>
+            <div className=' py-14 px-28 max-lg:px-[30px] -mx-28 max-lg:mx-[-30px]'>
+              <ProjectsGrid
+                projects={data?.searchProjects?.results ?? []}
+                nextCursor={data?.searchProjects?.nextCursor}
+                onRefetch={onRefetch}
+                loading={loading}
+              />
+            </div>
+          </>
+        )}
+        {data?.searchProjects?.totalCount === 0 && called && (
+          <div className='flex flex-col gap-2 items-center'>
+            <p className='text-[22px]'>{t('no-results')}</p>
+            <p className='text-lg text-grey-light'>
+              {t('no-results-description')}
             </p>
           </div>
-          <div className='bg-grey-dark py-14 px-28 max-lg:px-[30px] -mx-28 max-lg:mx-[-30px]'>
-            <ProjectsGrid
-              projects={data?.searchProjects?.results ?? []}
-              nextCursor={data?.searchProjects?.nextCursor}
-              onRefetch={onRefetch}
-              loading={loading}
-            />
-          </div>
-        </>
-      )}
-      {data?.searchProjects?.totalCount === 0 && called && (
-        <div className='flex flex-col gap-2 items-center'>
-          <p className='text-[22px]'>{t('no-results')}</p>
-          <p className='text-lg text-grey-light'>
-            {t('no-results-description')}
-          </p>
-        </div>
-      )}
+        )}
 
-      <NextSeo title={t('seo-title')} />
+        <NextSeo title={t('seo-title')} />
+      </div>
     </div>
   );
 }
