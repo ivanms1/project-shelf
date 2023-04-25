3662ef6552d84f64581ba426f5449df399cecef2
miroz devkota <mirozxy@gmail.com>
2023-04-25 09:39:35 +0545

    crrop util file

---
+++ apps/web/src/components/Cropper/cropUtils.js
@@ -0,0 +1,26 @@
+export function getCroppedImg(imageSrc, pixelCrop) {
+  return new Promise((resolve) => {
+    const canvas = document.createElement('canvas');
+    canvas.width = pixelCrop.width;
+    canvas.height = pixelCrop.height;
+    const ctx = canvas.getContext('2d');
+
+    const image = new Image();
+    image.crossOrigin = 'anonymous';
+    image.src = imageSrc;
+    image.onload = function () {
+      ctx.drawImage(
+        image,
+        pixelCrop.x,
+        pixelCrop.y,
+        pixelCrop.width,
+        pixelCrop.height,
+        0,
+        0,
+        pixelCrop.width,
+        pixelCrop.height
+      );
+      resolve(canvas);
+    };
+  });
+}
