64f9a164d79f1c632584dbfa313448fe857ac284
Rakshit Pathak <rajatpathak435@gmail.com>
2023-04-28 08:22:03 +0530

    removed yup, added zod (#168)

--- apps/web/const/index.tsx
+++ apps/web/const/index.tsx
@@ -1,4 +1,4 @@
-import * as yup from 'yup';
+import * as zod from 'zod';
 
 import IndiaFlag from '@/assets/flags/india.svg';
 import EnglishFlag from '@/assets/flags/english.svg';
@@ -8,23 +8,34 @@ import SpainFlag from '@/assets/flags/spain.svg';
 
 export const TOKEN_NAME = 'project-shelf';
 
-export const projectValidationSchema = yup.object().shape({
-  title: yup.string().required('This is a required field'),
-  description: yup.string().required('This is a required field'),
-  repoLink: yup
-    .string()
-    .url('It must be a valid URL')
-    .required('This is a required field'),
-  siteLink: yup
-    .string()
-    .url('It must be a valid URL')
-    .required('This is a required field'),
-  tags: yup
+const REQUIRED_ERROR = 'This is is a required field';
+
+export const projectValidationSchema = zod.object({
+  title: zod.string(),
+  description: zod.string({
+    required_error: REQUIRED_ERROR,
+  }),
+  repoLink: zod
+    .string({
+      required_error: REQUIRED_ERROR,
+    })
+    .url('It must be a valid URL'),
+  siteLink: zod
+    .string({
+      required_error: REQUIRED_ERROR,
+    })
+    .url('It must be a valid URL'),
+  tags: zod
+    .object(
+      { value: zod.string(), label: zod.string() },
+      {
+        required_error: 'Add at least one tag',
+      }
+    )
     .array()
-    .of(yup.object().shape({ value: yup.string(), label: yup.string() }))
     .min(1, 'Add at least one tag')
     .max(5, 'Add no more than five tags'),
-  preview: yup.string().required('This is a required field'),
+  preview: zod.string().or(zod.custom<File>((v) => v instanceof File)),
 });
 
 export const LANG_LOCALE_STORAGE_KEY = 'project-shelf-lang';
