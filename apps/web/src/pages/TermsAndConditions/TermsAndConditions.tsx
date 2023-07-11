f63ab4fc3028de52b7f74c9762e1d3eb373019b7
miroz devkota <mirozxy@gmail.com>
2023-07-11 15:56:06 +0545

    feat(fix): fixed the import of data from const file

--- apps/web/src/pages/TermsAndConditions/TermsAndConditions.tsx
+++ apps/web/src/pages/TermsAndConditions/TermsAndConditions.tsx
@@ -1,27 +1,31 @@
+import React from 'react';
 import { useTranslation } from 'next-i18next';
-import { data } from './data';
+import { TERMS_AND_CONDITIONS } from 'const';
+import { NextSeo } from 'next-seo';
 
 function TermsAndConditions() {
   const { t } = useTranslation('terms-and-conditions');
+
   return (
-    <div className=' bg-black  '>
-      <div className=' m-auto flex max-w-[1080px] flex-col  gap-8 py-10  '>
-        <p className=' text-center text-4xl font-semibold'>
+    <div className='bg-black'>
+      <div className='m-auto flex max-w-[1080px] flex-col gap-8 py-32'>
+        <p className='text-center text-4xl font-semibold'>
           {t('terms-and-conditions')}
         </p>
-        <p className='px-10  text-gray-100 lg:px-[100px]'>
+        <p className='px-10 text-gray-100 lg:px-[100px]'>
           {t('terms-and-conditions-content')}
         </p>
 
-        {data.map((content, i) => (
+        {TERMS_AND_CONDITIONS.map((content, i) => (
           <div key={i} className='px-10 lg:px-[100px]'>
-            <p className=' text-xl font-semibold'>
+            <p className='text-xl font-semibold'>
               {i + 1}. {t(content.terms_and_conditions_heading)}
             </p>
-            <p className=''>{t(content.terms_and_conditions_content)}</p>
+            <p>{t(content.terms_and_conditions_content)}</p>
           </div>
         ))}
       </div>
+      <NextSeo title={t('terms-and-conditions')} />
     </div>
   );
 }
