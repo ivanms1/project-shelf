8ddc4e787ed733a71336ee19636a53ac398db09a
miroz devkota <mirozxy@gmail.com>
2023-07-11 15:56:52 +0545

    feat(seo): added next seo for the pages

--- apps/web/src/pages/PrivacyPolicy/PrivacyPolicy.tsx
+++ apps/web/src/pages/PrivacyPolicy/PrivacyPolicy.tsx
@@ -1,24 +1,27 @@
+import React from 'react';
 import { useTranslation } from 'next-i18next';
+import { NextSeo } from 'next-seo';
+
+const arr = Array.from(Array(8).keys()).map((_, index) => index + 1);
 
 function PrivacyPolicy() {
-  const arr = Array.from(Array(8).keys()).map((_, index) => index + 1);
-
   const { t } = useTranslation('privacy-policy');
   return (
-    <div className=' bg-black  '>
-      <div className=' m-auto flex max-w-[1080px] flex-col items-center gap-8 py-10  '>
-        <p className=' text-4xl font-semibold'>{t('privacy-policy')}</p>
+    <div className='bg-black'>
+      <div className='m-auto flex max-w-[1080px] flex-col items-center gap-8 py-32'>
+        <p className='text-4xl font-semibold'>{t('privacy-policy')}</p>
         <div className='flex flex-col gap-6'>
           {arr.map((paragraph) => (
             <p
               key={paragraph}
-              className='px-10 text-left  text-gray-100 lg:px-[100px]'
+              className='px-10 text-left text-gray-100 lg:px-[100px]'
             >
               {t(`privacy-policy-content-p${paragraph}`)}
             </p>
           ))}
         </div>
       </div>
+      <NextSeo title={t('privacy-policy')} />
     </div>
   );
 }
