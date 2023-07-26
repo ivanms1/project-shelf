2d263db4f7114f5ca76f96c0b58aaf9ec29bae63
miroz devkota <mirozxy@gmail.com>
2023-07-26 14:53:39 +0545

    fix(authRoute): fix route being seen by implementing check in SSR

--- apps/web/pages/create-project/index.tsx
+++ apps/web/pages/create-project/index.tsx
@@ -4,18 +4,38 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import { EN_LOCALE } from 'const';
 
-import type { GetStaticProps } from 'next';
+import type {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from 'next';
+import { getSession } from 'next-auth/react';
 
 export default CreateProject;
 
-export const getStaticProps: GetStaticProps = async ({ locale }) => {
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+): Promise<GetServerSidePropsResult<any>> => {
+  const session = await getSession(context);
+
+  if (!session || !session.user) {
+    return {
+      redirect: {
+        destination: '/', // Redirect to the home page or an unauthorized access page
+        permanent: false,
+      },
+    };
+  }
+
+  // Include translations using serverSideTranslations
+  const translations = await serverSideTranslations(
+    context.locale || EN_LOCALE,
+    ['create-project', 'project-form', 'common']
+  );
+
   return {
     props: {
-      ...(await serverSideTranslations(locale || EN_LOCALE, [
-        'create-project',
-        'project-form',
-        'common',
-      ])),
+      ...translations,
     },
   };
 };
