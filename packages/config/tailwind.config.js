99c6a239bd8d595e82fb2a2f95a28859c429fc03
miroz devkota <mirozxy@gmail.com>
2023-08-13 11:36:59 +0545

    feat(admin): add poppins font

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -2,49 +2,52 @@
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
       colors: {
-        primary: '#A259FF',
-        'primary-dark': '#9240FD',
-        'blue-light': '#377DF7',
-        black: '#2B2B2B',
-        'grey-dark': '#3B3B3B',
-        'grey-lighter': '#9e9ea7',
-        'grey-light': '#858584',
-        white: '#FFFFFF',
-        'red-light': '#FF6250',
-        silver: '#CCCCCC',
-        'pink-light': '#ea4c89',
-        blue: '#3f51b5',
-        'dark-blue': '#242731',
+        primary: "#A259FF",
+        "primary-dark": "#9240FD",
+        "blue-light": "#377DF7",
+        black: "#2B2B2B",
+        "grey-dark": "#3B3B3B",
+        "grey-lighter": "#9e9ea7",
+        "grey-light": "#858584",
+        white: "#FFFFFF",
+        "red-light": "#FF6250",
+        silver: "#CCCCCC",
+        "pink-light": "#ea4c89",
+        blue: "#3f51b5",
+        "dark-blue": "#242731",
       },
       backgroundImage: {
-        'gradient-primary':
-          'linear-gradient(128.15deg, #A259FF 49.75%, #377DF7 136.56%);',
-        'gradient-secondary':
-          'linear-gradient(100.92deg, #A259FF 13.57%, #FF6250 97.65%);',
-        'step-icon':
-          'linear-gradient(101.76deg, #A259FF 11.55%, #3B3B3B 77.45%);',
+        "gradient-primary":
+          "linear-gradient(128.15deg, #A259FF 49.75%, #377DF7 136.56%);",
+        "gradient-secondary":
+          "linear-gradient(100.92deg, #A259FF 13.57%, #FF6250 97.65%);",
+        "step-icon":
+          "linear-gradient(101.76deg, #A259FF 11.55%, #3B3B3B 77.45%);",
       },
       zIndex: {
-        'loading-overlay': '999',
-        'modal-overlay': '99999',
+        "loading-overlay": "999",
+        "modal-overlay": "99999",
       },
       fontFamily: {
-        mono: ['"Space Mono"', 'monospace'],
+        mono: ['"Space Mono"', "monospace"],
       },
       borderRadius: {
-        sm: '8px',
-        md: '12px',
-        lg: '20px',
-        xl: '30px',
-        circle: '50%',
+        sm: "8px",
+        md: "12px",
+        lg: "20px",
+        xl: "30px",
+        circle: "50%",
       },
     },
   },
   content: [
-    '../../packages/ui/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './src/**/*.{js,ts,jsx,tsx}',
+    "../../packages/ui/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   plugins: [],
 };
