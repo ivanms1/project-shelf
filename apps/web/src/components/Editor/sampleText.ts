8a95317b8439b4c2cfd55e4f8d9b7da092e416c1
Rakshit Pathak <rajatpathak435@gmail.com>
2023-08-16 14:56:19 +0530

    fix: deleted sample text file to fix build error

--- apps/web/src/components/Editor/sampleText.ts
+++
@@ -1,8 +0,0 @@
-import { $createParagraphNode, $createTextNode, $getRoot } from 'lexical';
-import { $createListItemNode, $createListNode } from '@lexical/list';
-import { $createHeadingNode, $createQuoteNode } from '@lexical/rich-text';
-import { $createLinkNode } from '@lexical/link';
-
-export default function prepopulatedText() {
-  const root = $getRoot();
-}
